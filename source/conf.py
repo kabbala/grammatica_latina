cdb8b2788e7c26382e7a30cca4c8fe62ef8ab033
kabbala <kabbala@users.noreply.github.com>
2018-04-29 01:57:57 +0900

    Read the Docs 테마로 바꿈

--- source/conf.py
+++ source/conf.py
@@ -76,17 +76,18 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'font_family': 'Noto Sans, Source Han Sans, sans-serif',
-    'font_size': '15px',
-    'head_font_family': 'sans-serif'
-}
+# html_theme_options = {
+#    'font_family': 'Noto Sans, Source Han Sans, sans-serif',
+#    'font_size': '15px',
+#    'head_font_family': 'sans-serif'
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
